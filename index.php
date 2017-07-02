15673b0042d90a79f5fbd34491a18cf2cc1b2456
LeonardoManceroMafares <leonardo_mancero@hotmail.com>
2017-07-02 17:04:01 -0500

    pop up

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>POPUP</title>
+<link rel="stylesheet" href="demo.css">
+<script>!window.jQuery && document.write(unescape('%3Cscript src="jquery-1.7.1.min.js"%3E%3C/script%3E'))</script>
+<script type="text/javascript" src="demo.js"></script>
+
+</head>
+				<body>
+				
+						<h2>POPUP</h2>
+						<form action="" method="post">
+						<input type="button" value="POP UP JS Y CSS" class="button" data-type="zoomin">
+						
+									
+									<div class="overlay-container" >
+									<div class="window-container zoomin" >
+									<div align="center" >
+									<h1>LOGIN</h1>
+									<input type="text" class="caja"><br><br>
+									<input type="text" class="caja"><br><br>
+									</div>
+									<input type="submit" value="ENVIAR" class="enviar">
+									<input type="button" value="CERRAR" class="close">
+									
+									</div>
+									
+									
+									<div class="window-container zoomout">
+									<h3>Versión en Zoom out </h3>
+									Texto de la ventana emergente<br/>
+									<br/>
+									<span class="close">Cerrar</span>
+									</div>
+									</div>
+				</form>
+				</body>
+</html>
